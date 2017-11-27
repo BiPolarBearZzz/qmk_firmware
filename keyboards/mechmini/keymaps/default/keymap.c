b607fa67d938b4bb0905655374e9fcb15c862f35
BiPolarBearZzz <30383140+BiPolarBearZzz@users.noreply.github.com>
2017-11-27 01:47:03 -0800

    BiPolarBearMini
    JustMine

--- keyboards/mechmini/keymaps/default/keymap.c
+++ keyboards/mechmini/keymaps/default/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_ESC,     KC_Q,       KC_W,       KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
      KC_TAB,     KC_A,       KC_S,       KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_ENT,
      KC_LSFT,    KC_Z,       KC_X,       KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   MO(_FN2),
-     KC_LCTL,    KC_LGUI,    KC_LALT,         _____,         KC_SPC,        _____,                   MO(_FN1), MO(_FN3)
+     KC_LCTL,    KC_LGUI,    KC_LALT,LT(_FN1, KC_SPC),         _____,        LT(_FN2, KC_SPC,          MO(_FN1), MO(_FN3)
    ),
    [_FN1] = KEYMAP(
      KC_GRAVE,   _____,      KC_UP,      KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MRWD,  KC_MPLY,  KC_MFFD,  KC_SLCK,  KC_PAUS,  KC_DEL,
